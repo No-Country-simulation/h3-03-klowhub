a83193a84639a6058865ab1bcd5d159b2b6b1508
MartinKun <martinkunbrc@hotmail.com>
2024-11-17 15:01:00 -0300

    feat: add optional showDetails prop to RatingProps

--- frontend/src/components/rating/rating.component.tsx
+++ frontend/src/components/rating/rating.component.tsx
@@ -1,33 +1,32 @@
 import Icon from "../icon/icon.component";
 
 type RatingProps = {
-  rating: number
-  ratingCount: number
-}
-
-const Rating = ({ rating, ratingCount }: RatingProps) => {
-  return (
-    <div className="flex gap-4">
-      <span>{ rating }</span>
-      <div className="relative">
-        <div className="flex">
-          {
-            Array(Math.floor(5)).fill(0).map((s, idx) => (
-              <Icon key={`unrated-star-${idx}`} name="star" /> 
-            ))
-          }
-        </div>
-        <div className="flex absolute left-0 top-0">
-          {
-            Array(Math.floor(rating)).fill(0).map((s, idx) => (
-              <Icon key={`unrated-star-${idx}`} name="rated-star" />
-            ))
-          }
-        </div>
-      </div>
-      <span>({ ratingCount })</span>
-    </div>
-  )
+  rating: number;
+  ratingCount: number;
+  showDetails?: boolean;
 };
 
-export default Rating
+const Rating = ({ rating, ratingCount, showDetails = true }: RatingProps) => {
+  return (
+    <div className="flex gap-4">
+      {showDetails && <span>{rating}</span>}
+
+      <div className="relative">
+        <div className="flex">
+          {Array.from({ length: 5 }).map((_, idx) => (
+            <Icon key={`unrated-star-${idx}`} name="star" />
+          ))}
+        </div>
+        <div className="flex absolute left-0 top-0">
+          {Array.from({ length: Math.floor(rating) }).map((_, idx) => (
+            <Icon key={`rated-star-${idx}`} name="rated-star" />
+          ))}
+        </div>
+      </div>
+
+      {showDetails && <span>({ratingCount})</span>}
+    </div>
+  );
+};
+
+export default Rating;
