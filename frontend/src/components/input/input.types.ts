61f25c3170e79b0c751599cfc72db73c3fcbd6f0
GhostOrder28 <jtrvic7@gmail.com>
2024-11-20 22:13:32 -0500

    bugfix: input.component now can handle any form types as long as you add those to the corresponding OneOf types

--- frontend/src/components/input/input.types.ts
+++ frontend/src/components/input/input.types.ts
@@ -1,16 +1,21 @@
-import { UseFormRegister, FieldErrors, Control } from "react-hook-form";
-import { CourseInfo } from "@/types/courses.types";
+import { UseFormRegister, FieldErrors, Control, FieldValues } from "react-hook-form";
+import { CourseInfo, CourseDetails } from "@/types/courses.types";
 import { OneOf } from "@/types/utils.types";
 import { OptionsOrGroups, GroupBase } from "react-select";
+import { Expand } from "@/types/utils.types";
 
 type FieldType = "text" | "textarea" | "number" | "checkbox" | "date" | "time" | "password";
-type FieldName = keyof (CourseInfo) // this type is not strict but to make it strict we will need refactor too much things and rn it doesn't make sense to do that
-type FormErrors = OneOf<[ FieldErrors<CourseInfo> ]>
+type FieldName = Expand<keyof (CourseInfo & CourseDetails)> // this type is not strict but to make it strict we will need refactor too much things and rn it doesn't make sense to do that
+
+// IMPORTANT: every new form should be added in these types
+type FormErrors = OneOf<[ FieldErrors<CourseInfo>, FieldErrors<CourseDetails> ]>
+type FormFields = OneOf<[ UseFormRegister<CourseInfo>, UseFormRegister<CourseDetails> ]>
+type ControlledFields = OneOf<[ Control<CourseInfo>, Control<CourseDetails> ]>
 
 type CommonProps = {
   name: FieldName
   label: string
-  register: UseFormRegister<OneOf<[ CourseInfo ]>>
+  register: FormFields
   errors: FormErrors
   placeholder?: string
   className?: string
@@ -26,7 +31,7 @@ type RadioInput = {
 } & Omit<CommonProps, "placeholder">
 
 type ControlledInput = {
-  control: Control<CourseInfo>
+  control: ControlledFields
 } & CommonProps
 
 type RichTextInput = {
@@ -40,7 +45,6 @@ export type SelectOption = {
 
 type SelectInput = {
   type: "select"
-  // options: SelectOption[]
   options?: OptionsOrGroups<SelectOption, GroupBase<SelectOption>> | undefined
   isMulti?: boolean
 } & ControlledInput
