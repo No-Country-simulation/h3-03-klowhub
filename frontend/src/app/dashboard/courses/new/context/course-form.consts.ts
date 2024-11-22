442211c3a7050b8fcdf84c9f97094738d0f6df48
GhostOrder28 <jtrvic7@gmail.com>
2024-11-22 11:57:06 -0500

    bugfix: import path corrected

--- frontend/src/app/dashboard/courses/new/context/course-form.consts.ts
+++ frontend/src/app/dashboard/courses/new/context/course-form.consts.ts
@@ -1,6 +1,6 @@
 import { Course } from "@/types/courses.types";
-import { COURSE_INFO_INITIAL_STATE } from "../_components/general-info-form/general-info-form.consts";
-import { COURSE_DETAILS_INITIAL_STATE } from "../_components/details-form/details-form.consts";
+import { COURSE_INFO_INITIAL_STATE } from "../components/general-info-form/general-info-form.consts";
+import { COURSE_DETAILS_INITIAL_STATE } from "../components/details-form/details-form.consts";
 
 export const COURSE_FORM_INITIAL_STATE: Course = {
   generalInfo: COURSE_INFO_INITIAL_STATE,
