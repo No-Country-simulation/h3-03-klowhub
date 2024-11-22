3fa84e76ef67a8e4ba19ae8710ba788178a128b1
GhostOrder28 <jtrvic7@gmail.com>
2024-11-22 11:24:47 -0500

    feat: details section finished, update: route-btn.component was added to all sections

--- frontend/src/app/dashboard/courses/new/_components/modules-form/modules-form.consts.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@/types/courses.types";
-
-export const MODULE_INITIAL_STATE: Module = {
-  title: "",
-  description: "",
-  lessons: []
-};
--- frontend/src/app/dashboard/courses/new/page.tsx
+++ frontend/src/app/dashboard/courses/new/page.tsx
@@ -1,48 +1,51 @@
 import { getQueryParams } from "@/utils/route.utils";
-import GeneralSection from "./_components/general-form/general-form.component";
-import DetailsSection from "./_components/details-form/details-form.component";
-import ModuleSection from "./_components/modules-form/modules-form.component";
-import PromotionsSection from "./_components/promotions/promotions.section";
+import GeneralSection from "./components/general-info-form/general-info-form.component";
+import DetailsSection from "./components/details-form/details-form.component";
+import ModuleSection from "./components/modules-form/modules-form.component";
+import PromotionsSection from "./components/promotions/promotions.section";
 import Tab from "@/components/tab/tab.component";
 import FormAdvice from "@/components/form-advice/form-advice.component";
 import { advices } from "./consts";
-import { Button } from "@/components/ui/button";
+import CourseCtxProvider from "./context/course-form.context";
 
 const CreateCoursePage = async () => {
   const { section } = await getQueryParams();
 
   return (
-    <main className="flex flex-col gap-5">
-      <div>
-        <Tab section="general">Información general</Tab>
-        <Tab section="details">Detalles del curso</Tab>
-        <Tab section="modules">Módulos y lecciones</Tab>
-        <Tab section="promotions">Promociones</Tab>
-      </div>
-      <div className="flex gap-40 bg-card rounded-lg p-6">
-        <div className="w-3/4">
-          { section === "general" && <GeneralSection /> }
-          { section === "details" && <DetailsSection /> }
-          { section === "modules" && <ModuleSection /> }
-          { section === "promotions" && <PromotionsSection /> }
+    <>
+      <main className="flex flex-col gap-5 relative mb-28">
+        <div>
+          <Tab section="general">Información general</Tab>
+          <Tab section="details">Detalles del curso</Tab>
+          <Tab section="modules">Módulos y lecciones</Tab>
+          <Tab section="promotions">Promociones</Tab>
         </div>
-        <div className="w-1/4">
-          { 
-            advices.map((adv, idx) => (
-              <FormAdvice 
-                key={`advice-${idx}`} 
-                src={adv.img.url} 
-                alt={adv.img.alt}
-                display={adv.section === section}
-                header={adv.header}
-                text={adv.text}
-              />
-            ))
-          }
+        <div className="flex gap-40 bg-card rounded-lg p-6">
+          <div className="w-3/4">
+            <CourseCtxProvider>
+              { section === "general" && <GeneralSection /> }
+              { section === "details" && <DetailsSection /> }
+              { section === "modules" && <ModuleSection /> }
+              { section === "promotions" && <PromotionsSection /> }
+            </CourseCtxProvider>
+          </div>
+          <div className="w-1/4">
+            { 
+              advices.map((adv, idx) => (
+                <FormAdvice 
+                  key={`advice-${idx}`} 
+                  src={adv.img.url} 
+                  alt={adv.img.alt}
+                  display={adv.section === section}
+                  header={adv.header}
+                  text={adv.text}
+                />
+              ))
+            }
+          </div>
         </div>
-      </div>
-      <Button className="px-14 self-end">Continuar</Button>
-  </main>
+      </main>
+    </>
   )
 
 };
