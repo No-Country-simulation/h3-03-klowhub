5a6dddabc168c50dd5e2bd76a3a490bae2afc2ee
GhostOrder28 <jtrvic7@gmail.com>
2024-11-23 19:48:26 -0500

    update: tab.component is more generic more

--- frontend/src/components/tab/tab.component.tsx
+++ frontend/src/components/tab/tab.component.tsx
@@ -1,19 +1,16 @@
-import { getQueryParams } from "@/utils/route.utils";
 import { ReactNode } from "react";
 
 type Props = {
-  section: string
+  active: boolean
   children: ReactNode
 }
 
-const Tab = async ({ children, section }: Props) => {
-  const queryParams = await getQueryParams();
-
+const Tab = ({ children, active }: Props) => {
   return (
     <span 
       className={`
         inline-block border-b-2 border-solid px-4 pb-2 font-bold text-sm
-        ${queryParams.section === section ? "text-primary-300 border-primary-300" : "border-white"}`
+        ${active ? "text-primary-300 border-primary-300" : "border-white"}`
       }
     >
       { children }
