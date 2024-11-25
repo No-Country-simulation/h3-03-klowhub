c8e8156de5b392c5ca0bd7b466264b3dadca0e7d
francogaray <francogaray.dev@gmail.com>
2024-11-25 14:54:12 -0300

    add: dependencies, fix: root api path

--- backend/ms-projects/src/projects/projects.module.ts
+++ backend/ms-projects/src/projects/projects.module.ts
@@ -3,9 +3,10 @@ import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity/project.entity';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project])],
+  imports: [TypeOrmModule.forFeature([Project]), HttpModule],
   providers: [ProjectsService],
   controllers: [ProjectsController]
 })
