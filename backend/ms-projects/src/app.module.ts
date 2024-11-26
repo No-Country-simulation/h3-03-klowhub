fb6321cdc2f4a9215a7d6e787a1d145ccea6777e
thomilopez <thomasignaciolopez@gmail.com>
2024-11-26 00:37:47 -0300

    configuracion de variables de entorno

--- backend/ms-projects/src/app.module.ts
+++ backend/ms-projects/src/app.module.ts
@@ -1,32 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ProjectsModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
 
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRoot({
-//       type: 'postgres',
-//       host: 'db',
-//       port: 5432,
-//       username: 'root',
-//       password: 'root',
-//       database: 'klowhub',
-//       autoLoadEntities: true,
-//       entities: [Application, Project],
-//       logging: true,
-//       synchronize: true,
-//     }),
-//     ProjectsModule
-//   ],
-
-// })
-// export class AppModule {}
--- backend/ms-projects/src/main.ts
+++ backend/ms-projects/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3002);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(envs.port);
+  logger.log(`App running on port ${envs.port}`);
+
+  // const app = await NestFactory.create(AppModule);
+  // app.useGlobalPipes(new ValidationPipe());
+  // await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
