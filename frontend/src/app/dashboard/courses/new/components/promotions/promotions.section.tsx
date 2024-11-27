f93e41f515b1d53f57c25782ce55148c00310a65
GhostOrder28 <jtrvic7@gmail.com>
2024-11-27 17:44:09 -0500

    refactor: course promotion section now uses the input component instead of vanilla html elements

--- frontend/src/app/dashboard/courses/new/components/promotions/promotions.section.tsx
+++ frontend/src/app/dashboard/courses/new/components/promotions/promotions.section.tsx
@@ -3,15 +3,15 @@
 import { useState, useEffect } from "react";
 import useUserContent from "./hooks/use-user-content.hook";
 import Tab from "@/components/tab/tab.component";
-import RouteBtn from "../route-btn/route-btn.component";
+import RouteBtn from "../../../../../../components/route-btn/route-btn.component";
+import { useWatch } from "react-hook-form";
 import useGenerateForm from "@/hooks/use-generate-form.hook";
 import { PROMOTION_INITIAL_STATE } from "./promotions.consts";
 import { useContext } from "react";
 import { CourseCtx } from "../../context/course-form.context";
-import { CoursePromotion } from "@/types/courses.types";
+import { Promotion } from "@/types/global.types";
 import Input from "@/components/input/input.component";
 import ProductCard from "@/components/product-card/product-card.component";
-import { Button } from "@/components/ui/button";
 import { setPromotionData } from "../../context/course-form.actions";
 
 type ContentType = "applications" | "courses"
@@ -29,7 +29,15 @@ const PromotionsSection = () => {
     handleSubmit,
     reset,
     formState: { isDirty }
-  } = useGenerateForm<CoursePromotion>(PROMOTION_INITIAL_STATE, state.promotion);
+  } = useGenerateForm<Promotion>(PROMOTION_INITIAL_STATE, state.promotion || PROMOTION_INITIAL_STATE);
+
+  const { controlledCommonProps: selectionProps } = useGenerateForm<{ selection: string }>({ selection: "yes" }, { selection: "yes" });
+
+  const selection = useWatch({
+    name: "selection",
+    control: selectionProps.control,
+    defaultValue: "yes"
+  });
 
   const [ showSelector, setShowSelector ] = useState(true);
   const [ contentType, setContentType] = useState<ContentType>('applications');
@@ -39,6 +47,11 @@ const PromotionsSection = () => {
     reset()
   }, [contentType, reset, showSelector])
 
+  useEffect(() => {
+    if (selection === "yes") setShowSelector(true);
+    if (selection === "no") setShowSelector(false);
+  }, [selection])
+
   return (
     <>
       <div className="flex flex-col gap-5">
@@ -46,19 +59,15 @@ const PromotionsSection = () => {
           <h3 className="font-bold">Fusiona tus cursos y apps, expande tus posibilidades</h3>
           <p>En AppSheetHub, te damos la libertad de combinar tus aplicaciones y cursos para crear soluciones únicas y personalizadas. No te limites a una sola herramienta: potencia tu creatividad uniendo conocimientos y funcionalidades para lograr un impacto mayor. Diseña, comparte y aprende como nunca antes. ¡El límite lo pones vos!</p>
         </div>
-        <div className="flex flex-col gap-5">
-          <h3 className="font-bold">¿Te gustaría ofrecer un descuento en alguno de tus otros productos al comprar este artículo?</h3>
-          <div>
-            <div className="flex gap-3">
-              <input type="radio" name="promotion" onChange={() => setShowSelector(true)} checked={showSelector} />
-              <label htmlFor="promotion">Sí</label>
-            </div>
-            <div className="flex gap-3">
-              <input type="radio" name="promotion" onChange={() => setShowSelector(false)} checked={!showSelector} />
-              <label htmlFor="promotion">No</label>
-            </div>
-          </div>
-        </div>
+        <Input 
+          name="selection"
+          type="radio-group"
+          options={[ 
+            { value: "yes", label: "Sí" },
+            { value: "no", label: "No" },
+          ]} 
+          { ...selectionProps }
+        />
         { showSelector &&
           <div className="
             rounded-lg flex flex-col gap-5
