8bc40fff08ec1784b1f9d009b8ebf7677d63bd64
GhostOrder28 <jtrvic7@gmail.com>
2024-11-29 20:56:41 -0500

    feat: greater component finished

--- frontend/src/components/greeter/greeter.component.tsx
+++ frontend/src/components/greeter/greeter.component.tsx
@@ -1,17 +1,26 @@
+import { ReactNode } from "react";
 import Overlay from "../overlay/overlay.component";
-import { X } from "lucide-react";
+import Icon from "../icon/icon.component";
 
-const Greeter = () => {
+type Props = {
+  header: string
+  message: string
+  children: ReactNode[]
+}
+
+const Greeter = ({ header, message, children }: Props) => {
   return (
-    <div className="p-">
+    <>
       <Overlay />
-      <X />
-      <div>
-        <h2>¡Felicitaciones! Tu curso/lección se publicó con éxito</h2>
-        <p>Ya está disponible para que estudiantes de todo el mundo lo descubran y aprovechen.</p>
-        <div></div>
+      <div className="fixed w-full h-screen top-0 left-0 z-[99999] flex justify-center items-center px-5 md:px-10">
+        <div className="py-10 px-5 md:px-16 bg-card flex flex-col gap-5 rounded-xl w-full md:w-[500px]">
+          <h2 className="font-bold text-center">{ header }</h2>
+          <p className="text-center">{ message }</p>
+          <div className="mx-auto"><Icon name="greeter-check" /></div>
+          <div className="mx-auto flex flex-col gap-5">{ children }</div>
+        </div>
       </div>
-    </div>
+    </>
   )   
 };
 
