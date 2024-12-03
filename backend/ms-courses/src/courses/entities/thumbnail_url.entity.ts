a8a5de79d3bf097b98da8c660f34a7f9fef7cbc3
javiercolodro <javiercolodro@gmail.com>
2024-12-03 16:53:52 -0300

    add multimedia endpoint

--- backend/ms-courses/src/courses/entities/thumbnail_url.entity.ts
+++ backend/ms-courses/src/courses/entities/thumbnail_url.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Video } from './video.entity';
 
 @Entity()
-export class Imagen {
+export class Thumbnail {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
