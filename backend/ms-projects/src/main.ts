2d36023cd839c7ff9de88c335933861711f3a84e
thomilopez <thomasignaciolopez@gmail.com>
2024-12-10 20:34:47 -0300

    Se agrego cors en main.ts

--- backend/ms-projects/src/main.ts
+++ backend/ms-projects/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  app.enableCors();
   const config = new DocumentBuilder()
   .setTitle('Klowhub API')
   .setDescription('Microservicio de usuarios')
