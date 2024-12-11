113eac6a898ee7d95447b29729bb8190394a3ba8
MartinKun <martinkunbrc@hotmail.com>
2024-12-10 23:07:27 -0300

    feat: updates were added

--- frontend/src/app/(auth)/register/sections/register-form.section.tsx
+++ frontend/src/app/(auth)/register/sections/register-form.section.tsx
@@ -38,50 +38,61 @@ const RegisterForm = () => {
     return (
         <form onSubmit={handleSubmit(customSubmit)} className="flex flex-col">
             <div className="flex flex-col pb-5 gap-3 lg:w-[350px] w-full mx-auto text-gray-100">
-                <input type="text"
-                    placeholder="Nombre completo"
-                    autoComplete="off"
-                    {...register('fullname', { required: true })} className="w-full h-[45px] p-3 rounded-lg" />
-                {errors.fullname?.type === 'required' && (
-                    <p className="w-full text-[#cd5c5c] font-semibold text-sm">
-                        Complete este campo
-                    </p>
-                )}
-                <input type="email"
-                    placeholder="Correo Electrónico"
-                    autoComplete="off"
-                    {...register("email", {
-                        required: true,
-                        pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
-                    })}
-                    className="w-full h-[45px] p-3 rounded-lg" />
-                {errors.email?.type === 'pattern' && (
-                    <p className="w-full text-[#cd5c5c] font-semibold text-sm">
-                        No es un email válido
-                    </p>
-                )}
-                {errors.email?.type === 'required' && (
-                    <p className="w-full text-[#cd5c5c] font-semibold text-sm">
-                        Complete este campo
-                    </p>
-                )}
-                <input type="password"
-                    placeholder="Contraseña"
-                    {...register('password', {
-                        required: true,
-                        pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                    })}
-                    className="w-full h-[45px] p-3 rounded-lg" />
-                {errors.password?.type === 'pattern' && (
-                    <p className="w-full text-[#cd5c5c] font-semibold text-sm">
-                        La contraseña debe tener más de 8 caracteres, incluir una mayúscula, un número y un carácter especial.
-                    </p>
-                )}
-                {errors.password?.type === 'required' && (
-                    <p className="w-full text-[#cd5c5c] font-semibold text-sm">
-                        Complete este campo
-                    </p>
-                )}
+                <div className='relative'>
+                    <input type="text"
+                        placeholder="Nombre completo"
+                        autoComplete="off"
+                        {...register('fullname', { required: true })} className={`w-full h-[45px] p-3 rounded-lg`} />
+                    {errors.fullname?.type === 'required' && (
+                        <div className="z-10 lg:right-6 right-4 top-12 absolute w-full text-[#ffffff] font-semibold text-sm bg-card p-3.5 rounded-md flex gap-5">
+                            <div className="absolute -top-2 left-4 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[10px] border-b-card"></div>
+                            <p>Complete este campo</p>
+                        </div>
+                    )}
+                </div>
+                <div className='relative'>
+                    <input type="email"
+                        placeholder="Correo Electrónico"
+                        autoComplete="off"
+                        {...register("email", {
+                            required: true,
+                            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+                        })}
+                        className="w-full h-[45px] p-3 rounded-lg" />
+                    {errors.email?.type === 'pattern' && (
+                        <div className="z-10 lg:right-6 right-4 top-12 absolute w-full text-[#ffffff] font-semibold text-sm bg-card p-3.5 rounded-md flex gap-5">
+                            <div className="absolute -top-2 left-4 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[10px] border-b-card"></div>
+                            <p>No es un email válido</p>
+                        </div>
+                    )}
+                    {errors.email?.type === 'required' && (
+                        <div className="z-10 lg:right-6 right-4 top-12 absolute w-full text-[#ffffff] font-semibold text-sm bg-card p-3.5 rounded-md flex gap-5">
+                            <div className="absolute -top-2 left-4 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[10px] border-b-card"></div>
+                            <p>Complete este campo</p>
+                        </div>
+                    )}
+                </div>
+                <div className='relative'>
+                    <input type="password"
+                        placeholder="Contraseña"
+                        {...register('password', {
+                            required: true,
+                            pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                        })}
+                        className="w-full h-[45px] p-3 rounded-lg" />
+                    {errors.password?.type === 'pattern' && (
+                        <div className="z-10 lg:right-6 right-4 top-12 absolute w-full text-[#ffffff] font-semibold text-sm bg-card p-3.5 rounded-md flex gap-5">
+                            <div className="absolute -top-2 left-4 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[10px] border-b-card"></div>
+                            <p>La contraseña debe tener más de 8 caracteres, incluir una mayúscula, un número y un carácter especial.</p>
+                        </div>
+                    )}
+                    {errors.password?.type === 'required' && (
+                        <div className="z-10 lg:right-6 right-4 top-12 absolute w-full text-[#ffffff] font-semibold text-sm bg-card p-3.5 rounded-md flex gap-5">
+                            <div className="absolute -top-2 left-4 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[10px] border-b-card"></div>
+                            <p>Complete este campo</p>
+                        </div>
+                    )}
+                </div>
             </div>
             <div className="w-full p-4 flex flex-wrap gap-2 text-xs justify-center">
                 <span>Al registrarte, aceptas nuestras</span>
