7a24459c0b5439496668835e1bd4343c842ef198
MartinKun <martinkunbrc@hotmail.com>
2024-12-15 09:53:29 -0300

    chore: reglace axios with fetch for API calls

--- frontend/src/hooks/use-fetch-data.hook.ts
+++ frontend/src/hooks/use-fetch-data.hook.ts
@@ -2,15 +2,20 @@ import services from "@/services";
 import { ServiceTypes } from "@/types/service-types";
 
 const useFetchData = () => {
-  const fetchData = async <T extends keyof ServiceTypes>(service: T, data: ServiceTypes[T]) => {
+  const fetchData = async <T extends keyof ServiceTypes>(
+    service: T,
+    data: ServiceTypes[T]
+  ) => {
     try {
       const response = await services[service](data);
-      return { status: true, response: response.data };
-    } catch (error) {
-      return { status: false, error };
+      return { status: true, response };
+    } catch (error: any) {
+      return { status: false, error: error.message || 'Error desconocido' };
     }
   };
+
   return { fetchData };
 };
 
 export default useFetchData;
+
--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,21 +1,37 @@
-import axios from "axios";
+const LOGIN_URL = `${process.env.NEXT_PUBLIC_AUTH_URL}/login`
+const REGISTER_URL = `${process.env.NEXT_PUBLIC_AUTH_URL}/register`
 
-const api = axios.create({
-    // url que viene del .env o el local
-    baseURL: process.env.NEXT_PUBLIC_AUTH_URL, 
+const apiFetch = async (url: string, options?: RequestInit) => {
+  const response = await fetch(`${url}`, {
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-    // withCredentials:true
+    ...options,
   });
 
-
-  const login = async <T>(data: T) => await api.post('/login', data)
-  const register = async <T>(data: T) => await api.post('/register', data)
-
-const services = {
-    login,
-    register
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || 'Error al realizar la solicitud');
   }
 
-  export default services;
+  return response.json();
+};
+
+const login = async <T>(data: T) =>
+  apiFetch(LOGIN_URL, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+
+const register = async <T>(data: T) =>
+  apiFetch(REGISTER_URL, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+
+const services = {
+  login,
+  register,
+};
+
+export default services;
