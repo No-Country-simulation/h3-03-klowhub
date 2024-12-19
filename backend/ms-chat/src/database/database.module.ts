bf62559a56487d351cc7be951fca60310f236138
thomilopez <thomasignaciolopez@gmail.com>
2024-12-19 00:02:54 -0300

    config de socket.io en ms-chat

--- backend/ms-chat/src/database/database.module.ts
+++ backend/ms-chat/src/database/database.module.ts
@@ -2,19 +2,19 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { envs } from "src/config";
 import { Message } from "src/chat/entities/message.entity";
-
+import { Chat } from "src/chat/entities/chat.entity";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: '',
-      host: envs.dbHost || 'db', // Cambia esto si es necesario
-      port: envs.dbPort || 5432,
-      username: envs.dbUsername || 'root', // Cambia a tu usuario de PostgreSQL
-      password: envs.dbPassword || 'root', // Cambia a tu contraseña de PostgreSQL
-      database: envs.dbDatabaseName || 'klowhub', // Nombre de tu base de datos
-      entities: [Message],
+      host: envs.dbHost, 
+      port: envs.dbPort,
+      username: envs.dbUsername, 
+      password: envs.dbPassword,
+      database: envs.dbDatabaseName, 
+      entities: [Message, Chat],
       synchronize: true, // No usar en producción
       logging: true,
       // ssl: {
